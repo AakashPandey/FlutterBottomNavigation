dc6f25ddbf578b3e00c7baac1f2dee7ffdd1439e
Aakash Pandey <Aakash.pandey@live.com>
2019-04-27 06:36:01 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BottomNavDemo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        brightness: Brightness.dark,
+        accentColor: Colors.redAccent,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Widget> pageWidgets = [
+    Text("One"),
+    Text("Two"),
+    Text("Three"),
+    Text("Four"),
+    Text("Five")
+  ];
+
+  int pg = 0;
+  List pgStack = [];
+
+  void pgPush(i) {
+    if (pgStack.isEmpty) {
+      pgStack.add(pg);
+    }
+
+    if (i == pg) {
+      return;
+    }
+
+    if (i == pgStack[0]) {
+      pg = i;
+    }
+
+    if (!pgStack.contains(pg)) {
+      pgStack.add(pg);
+    }
+
+    setState(() {
+      pg = i;
+    });
+
+    print(pgStack.toString());
+  }
+
+  Future<bool> pgPop(BuildContext context) {
+    print("called " + pgStack.toString());
+    if (pgStack.isEmpty) {
+      print("App terminated!");
+      return Future<bool>.value(true);
+    } else {
+      int t = pgStack.removeLast();
+      setState(() {
+        pg = (pg!=t) ? t : pgStack.removeLast(); 
+      });
+    }
+    print(pgStack.toString());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // handle back navigation
+    return WillPopScope(
+      onWillPop: () => pgPop(context),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text("BottomNavDemo"),
+        ),
+        body: Center(child: pageWidgets[pg]),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), title: Text("Home")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.flash_on), title: Text("Trends")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.subscriptions), title: Text("Subs")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.mail), title: Text("Inbox")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.folder), title: Text("Library")),
+          ],
+          currentIndex: pg,
+          type: BottomNavigationBarType.fixed,
+          onTap: (i) {
+            pgPush(i);
+          },
+        ),
+      ),
+    );
+  }
+}
